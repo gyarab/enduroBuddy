82d2ba4284fabd48bc85bd9aeb62303a8fcb706a
vojtecholcman <vojtech.holcman.s@gyarab.cz>
2026-01-25 17:00:02 +0100

    edited dahsboard.html-other, dashboard without boot, doable, dw month preclick

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path('', include('dashboard.urls')),
+    path("", include("dashboard.urls")),
 ]
+
