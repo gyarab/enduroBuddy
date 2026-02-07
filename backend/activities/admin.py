6f8a41ba8e2b01a666ff1fd6c9f6ae3bf99d8184
Vojtech Holcman <vojtech.holcman.s@gyarab.cz>
2026-02-07 22:39:52 +0100

    refactor: update activities app with new models and admin configurations, add fit file parser and signals

--- backend/activities/admin.py
+++ backend/activities/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
-from .models import Activity, ActivityInterval
+from .models import Activity, ActivityInterval, ActivityFile
+
+
+class ActivityIntervalInline(admin.TabularInline):
+    model = ActivityInterval
+    extra = 0
+
+
+class ActivityFileInline(admin.TabularInline):
+    model = ActivityFile
+    extra = 0
+
 
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ("id", "athlete", "sport", "started_at")
+    inlines = [ActivityFileInline, ActivityIntervalInline]
+
 
 @admin.register(ActivityInterval)
 class ActivityIntervalAdmin(admin.ModelAdmin):
     list_display = ("id", "activity", "index", "duration_s", "distance_m")
+
+
+@admin.register(ActivityFile)
+class ActivityFileAdmin(admin.ModelAdmin):
+    list_display = ("id", "activity", "file_type", "uploaded_at")
