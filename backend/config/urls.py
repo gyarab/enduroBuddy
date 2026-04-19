6693eea116649cfd7750cfa9cf9556e9d1744717
Vojtech Holcman <vojtech.holcman.s@gyarab.cz>
2026-04-19 10:43:40 +0200

    feat: implement Nuxt entry point view for redirection in migration

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.conf import settings
@@ -38,8 +22,6 @@ from accounts.views import (
     spa_social_login_error,
 )
 from config import error_views
-from config.views_public import public_about, public_home, public_privacy, public_terms
-from config.views_spa import public_spa_entry, spa_entry
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -66,20 +48,9 @@ urlpatterns = [
     path("accounts/social/login/error/", spa_social_login_error, name="socialaccount_login_error"),
     path("accounts/social/login/cancelled/", spa_social_login_cancelled, name="socialaccount_login_cancelled"),
     path("accounts/social/connections/", spa_social_connections, name="socialaccount_connections"),
-    path("app/profile/complete", spa_entry, name="account_complete_profile"),
     path("accounts/", include("allauth.urls")),
     path("__debug/ui/errors/", error_views.error_preview_index, name="error_preview_index"),
     path("__debug/ui/errors/<int:status_code>/", error_views.error_preview_status, name="error_preview_status"),
-    path("", public_home, name="public_home"),
-    path("about/", public_about, name="public_about"),
-    path("terms/", public_terms, name="public_terms"),
-    path("privacy/", public_privacy, name="public_privacy"),
-    path("auth-preview/", public_spa_entry, name="spa_auth_preview_root"),
-    re_path(r"^auth-preview/(?P<path>.*)$", public_spa_entry, name="spa_auth_preview"),
-    path("app/", spa_entry, name="spa_app_root"),
-    re_path(r"^app/(?P<path>.*)$", spa_entry, name="spa_app"),
-    path("coach/", spa_entry, name="spa_coach_root"),
-    re_path(r"^coach/(?P<path>.*)$", spa_entry, name="spa_coach"),
     path("", include("dashboard.urls")),
 ]
 
